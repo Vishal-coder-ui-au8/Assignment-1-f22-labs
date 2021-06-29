76b2e431016194c5e57f6b453798ce372d277a93
Vishal sreevaatsav <vishalvatsav@gmail.com>
2021-06-29 22:47:22 +0530

    c

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Oval from '../images/oval.png';
-import iphone from "../images/i-phone-body.jpg";
+import iphone from "../images/phone.png";
 import Path from "../images/path.png";
 import logo_1 from "../images/logo-1.jpg";
 import logo_2 from "../images/logo-2.jpg";
@@ -14,6 +14,20 @@ import chat from "../images/chat-content.png";
 import curve from "../images/white-curve-1.png";
 import rectangle from "../images/rectangle.png";
 import image from "../images/img.png";
+import bg4 from "../images/bg4.png";
+import Unique from "../images/50-unique-design-bl.png";
+import icon from "../images/icon-form4.png";
+import mobs from "../images/mobs.png";
+import shape from "../images/shape.png";
+import rect from "../images/rectangle-form4.png";
+import Path1 from "../images/path-form5.png";
+import bg6 from "../images/bg6.png";
+import twitter from "../images/path-form5.png";
+import facebook from "../images/facebook-image.png";
+import google from "../images/google-2-x.png"; 
+import Project from '../images/project-discussion-w.png';
+import bell from "../images/bell-53.png";
+import pro from "../images/you-ve-got-a-new-pro.png";
 
 class Header extends Component {
   render() {
@@ -114,16 +128,180 @@ class Header extends Component {
             With lots of unique blocks,<br/> you can easily build a page<br /> without coding.
             </div>
             <div className="Build-a-perfect-land">Build a perfect landing page fast.</div>
-            <div className="Create-custom-landin">Create custom landing pages with Omega that converts <br/>more visitors than any website. With lots of unique blocks, <br/>you can easily build a page without coding.</div>
+            <div className="Create-custom-landin-form2">Create custom landing pages with Omega that converts <br/>more visitors than any website. With lots of unique blocks, <br/>you can easily build a page without coding.</div>
             <div className="bg_form2"></div>
             <div className="Check-all-features">Check all features</div>
-            <div className="white-curve">
+            {/* <div className="white-curve-1">
                 <img src={curve}/>
-            </div>
+            </div> */}
         </div>
 
-        
-        
+        <div className="form-3">
+             <div className="Save-tons-of-time-on">Save tons of time<br/> on design & coding.</div>
+             <div className="Create-custom-landin-2">Create custom landing pages with Omega that converts <br/>more visitors than any website. With lots of unique blocks,<br/> you can easily build a page without coding.</div>
+             <div className="bg-started-3"></div>
+             <div className="Get-Started-3">Get Started</div>
+             <div className="image">
+                 <img src={image}/>
+             </div>
+             <div className="Rectangle">
+                 <img src={rectangle}/>
+             </div>
+             <div className="Rectangle-form3"></div>
+             <div className="Next-meeting-on-30-m">
+               Next meeting on 30 mins
+             </div>
+             <div className="Oval-form3"></div>
+             <div className="bell-form3">
+                <img src={bell}/>
+             </div>
+             <div className="Project-Discussion-w">
+                 <img src={Project}/>
+             </div>
+        </div>
+
+
+        {/* <div className="white-curve-2"></div>      */}
+        <div className="form-4">
+            <div className="Its-everything-you">
+                It’s everything<br/> you’ll ever need.
+            </div>
+            
+            <div className="bg4">
+                 <img src={bg4}/>
+             </div>
+             <div className="rectangle-form4">
+                <img src={rect}/>
+             </div>
+             <div className="Create-custom-landin-4">
+             Create custom landing pages with Omega that converts<br/> more visitors than any website. With lots of unique blocks, <br/>you can easily build a page without coding.
+             </div>
+             <div className="bg-form4"></div>
+             <div className="-Unique-Design-Bl">
+                 <div className="-Unique-Design-Bl .text-style-1">
+                   <img src={Unique}/>
+                 </div>
+             </div>
+             <div className="icon-form4">
+                <img src={icon}/>
+             </div>
+             <div className="bg2-form4"></div>
+             <div className="Mobile-First-Design">
+                 <div className="Mobile-First-Design .text-style-1">
+                    <img src={mobs}/>
+                 </div>
+             </div>
+             <div className="Oval-form4"></div>
+             <div className="Shape">
+               <img src={shape}/>
+             </div>
+             <div className="Rectangle-form4"></div>
+             <div className="Deadline-Oct-29-20">Deadline: Oct 29, 2020</div>
+             <div className="Oval2-form4"></div>
+             <div className="bell-53">
+                <img src={bell}/>
+             </div>
+             <div className="Youve-got-a-new-pro">
+                <img src={pro}/>
+             </div>
+        </div>  
+
+
+        <div className="form-5">
+           <div className="Customers-are-loving">
+              Customers are<br/> loving our Omega 
+           </div>
+           <div className="bg-form5"></div>
+           <div className="Jonathan-Taylor">Jonathon Taylor</div>
+           <div className="bg2-form5"></div>
+           <div className="Gavin-Park">Gavin Park</div>
+           <div className="bg3-form5"></div>
+           <div className="Betty-Norton">Betty Norton</div>
+           <div className="Path-form5">
+              <img src={Path1}/>
+           </div>
+           <div className="Path2-form5">
+              <img src={Path1}/>
+           </div>
+           <div className="Path3-form5">
+              <img src={Path1}/>
+           </div>
+           <div className="OMG-I-cannot-believ">
+              <div className="OMG-I-cannot-believ .text-style-1">
+              OMG! I cannot believe that I have <br/>got a brand new landing page<br/> after getting @Omega…
+              </div>
+           </div>
+           <div className="Omega-is-great-for">
+              <div className="Omega-is-great-for .text-style-1">
+              @Omega is great for creating <br/>landing pages within minutes! It actually takes less time…
+              </div>
+           </div>
+           <div className="Whenever-I-need-to-c">
+               <div className="Whenever-I-need-to-c .text-style-1">
+               Whenever I need to create a new landing page for clients, <br/>@Omega is my solution…
+               </div>
+           </div>
+           <div className="CEO-at-Creativex">CEO at Creativex</div>
+           <div className="CEO-at-Orbital">CEO at Orbital</div>
+           <div className="Freelance-Designer">Freelance Designer</div>
+        </div>
+
+{/*     <div className="White-curve-3"></div> */}
+
+        <div className="form-6">
+           <div className="bg-form6">
+              <img src={bg6}/>
+           </div>
+           <div className="Get-started-form6">
+             Get started
+           </div>
+           <div className="Create-custom-landin-form6">
+           Create custom landing pages with Omega that converts<br/> more visitors than any website. With lots of unique blocks, <br/>you can easily build a page without coding.
+           </div>
+           <div className="Rectangle-form6"></div>
+           <div className="Your-name">Your name</div>
+           <div className="Rectangle2-form6"></div>
+           <div className="Email-address">Email address</div>
+           <div className="bg2-form6"></div>
+           <div className="Get-Started-2-form6">
+             Get Started
+           </div>
+           <div className="Omega-form6">Omega</div>
+           <div className="About-form6">About</div>
+           <div className="Learn">Learn</div>
+           <div className="Stories">Stories</div>
+           <div className="Hire">Hire</div>
+           <div className="Our-mission-Our-stor">
+               Our mission<br/>
+               Our story<br/>
+               Team Members
+           </div>
+           <div className="Tutorials-How-it-wor">
+               Tutorials<br/>
+               How  it works<br/>
+               F.A.Q
+           </div>
+           <div className="Blog-Tech-stories">
+               Blog<br/>
+               Tech Stories
+           </div>
+           <div className="Career-Freelancers-T">
+               Career<br/>
+               Freelancers<br/>
+               Trainee
+           </div>
+           <div className="Line-2"></div>
+           <div className="UXTheme-All">© 2020 UXTheme, All Rights Reserved</div>
+           <div className="logo-twitter2x">
+              <img src={twitter}/>
+           </div>
+           <div className="logo-fb-simple2x">
+               <img src={facebook}/>
+           </div>
+           <div className="google2x">
+              <img src={google}/>
+           </div>
+        </div>
 
 
 
